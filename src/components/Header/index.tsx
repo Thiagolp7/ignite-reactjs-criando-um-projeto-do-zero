8f9fbd45950e0f6db70080c6461bf9183dc06977
ThiagoLp <thiagoluiz.ps@hotmail.com>
2022-03-30 10:58:44 -0300

    feat: create Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,17 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import styles from './header.module.scss'
+
 export default function Header() {
-  // TODO
+  return (
+    <header className={styles.headerContainer}>
+      <Link href="/">
+        <a>
+          <Image src="/images/Logo.svg" width="238.62px"
+            height="25.63px"
+          />
+        </a>
+      </Link>
+    </header>
+  )
 }
