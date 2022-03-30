b16487ebec35f9407528ad1b8bd4ae78ea2eb48e
ThiagoLp <thiagoluiz.ps@hotmail.com>
2022-03-30 11:40:06 -0300

    feat: add post page structure

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,9 +1,12 @@
+import Image from 'next/image'
 import { GetStaticPaths, GetStaticProps } from 'next';
 
 import { getPrismicClient } from '../../services/prismic';
 
 import commonStyles from '../../styles/common.module.scss';
 import styles from './post.module.scss';
+import { AiOutlineCalendar } from 'react-icons/ai';
+import { BiUser } from 'react-icons/bi';
 
 interface Post {
   first_publication_date: string | null;
@@ -26,9 +29,31 @@ interface PostProps {
   post: Post;
 }
 
-// export default function Post() {
-//   // TODO
-// }
+export default function Post() {
+  return (
+    <main className={styles.container}>
+      <div className={styles.banner}>
+        <Image src="/images/banner.png" width="1440px"
+          height="500px"
+        />
+      </div>
+      <div className={styles.content}>
+        <h1>Criando um app CRA do zero</h1>
+        <div>
+          <span>
+            <AiOutlineCalendar/>
+            19 Mar 2021
+          </span>
+          <span>
+            <BiUser/>
+            Joseph Oliveira
+          </span>
+        </div>
+
+      </div>
+    </main>
+  )
+}
 
 // export const getStaticPaths = async () => {
 //   const prismic = getPrismicClient();
