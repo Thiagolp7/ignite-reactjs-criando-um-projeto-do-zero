f4f913877d1afd0aa4d5e211bbdda44cdcf148b3
Thiagolp7 <thiagolp.dev@gmail.com>
2022-04-14 14:11:39 -0300

    feat: adjuste header size

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,14 +4,16 @@ import styles from './header.module.scss'
 
 export default function Header() {
   return (
-    <header className={styles.headerContainer}>
-      <Link href="/">
-        <a>
-          <Image src="/images/Logo.svg" width="238.62px"
-            height="25.63px"
-          />
-        </a>
-      </Link>
+    <header className={styles.header}>
+      <div className={styles.headerContainer}>
+        <Link href="/">
+          <a>
+            <Image src="/images/Logo.svg" width="238.62px"
+              height="25.63px"
+            />
+          </a>
+        </Link>
+      </div>
     </header>
   )
 }
